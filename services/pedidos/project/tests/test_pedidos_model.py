c1bcbead2d88acd1f916ff5d475a84dc3694bfc0
brandux <you@example.com>
2019-07-01 09:00:25 -0500

    travis ok

--- services/pedidos/project/tests/test_pedidos_model.py
+++ services/pedidos/project/tests/test_pedidos_model.py
@@ -6,15 +6,16 @@ import unittest
 from project import db
 from project.api.models import Customer
 from project.tests.base import BaseTestCase
-from project.tests.utils import add_customer
+# from project.tests.utils import add_customer
 
 from sqlalchemy.exc import IntegrityError
+
 
 class TestUserModel(BaseTestCase):
 
     def test_add_customer(self):
         customer = Customer(
-            name = 'brandux',
+            name='brandux'
         )
         # user = add_user('justatest', 'test@test.com')
         db.session.add(customer)
@@ -22,24 +23,22 @@ class TestUserModel(BaseTestCase):
         self.assertTrue(customer.id)
         self.assertEqual(customer.name, 'brandux')
 
-
     def test_add_customer_duplicate_name(self):
         customer = Customer(
-            name = 'brandux',
+            name='brandux'
         )
         # add_user('justatest', 'test@test.com')
         db.session.add(customer)
         db.session.commit()
         duplicate_customer = customer(
-            name='brandux',
+            name='brandux'
         )
         db.session.add(duplicate_customer)
         self.assertRaises(IntegrityError, db.session.commit)
 
-
     def test_to_json(self):
         customer = Customer(
-            name = 'brandux',
+            name='brandux'
         )
         # add_user('justatest', 'test@test.com')
         db.session.add(customer)
